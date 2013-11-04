03c41ecfe468c5ddec304329a3b0d01009e3958f
Guillermo Correa <luis.guillermo.correa@hotmail.com>
2013-11-04 08:50:44 -0500

    module2 adde console client app

---
+++ module2/NorthwindWCFDataServicesDemo/ConsoleOdataClientApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace ConsoleOdataClientApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            var result = client.GetAsync("http://localhost.fiddler:28510/NorthwindDataService.svc/Customers");
+            
+            Console.WriteLine("Press enter to exit.");
+            Console.ReadKey();
+        }
+    }
+}
